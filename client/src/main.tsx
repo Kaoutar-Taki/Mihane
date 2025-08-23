d6804ac28a6b4effa95e020747d48234336c44f3
Kaoutar-Taki <takikaoutar01@gmail.com>
2025-08-23 22:05:29 +0100

    clean code

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,19 +1,12 @@
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Upload from "./pages/Upload";
-import Validate from "./pages/Validate";
-import Cards from "./pages/Cards";
-import Layout from "./components/Layout";
+import "./i18n";
 import "./index.css";
+import "keen-slider/keen-slider.min.css";
+import App from "./app/App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Cards />} />
-        <Route path="upload" element={<Upload />} />
-        <Route path="validate" element={<Validate />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+  <StrictMode>
+    <App/>
+  </StrictMode>
 );
